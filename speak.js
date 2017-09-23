d4ac4a0d55031cec50c74ea049290d41c0479fb8
patrickzebinghe <patrick2hb@hotmail.com>
2017-09-23 16:35:49 -0400

    Add files via upload

--- speak.js
+++ speak.js
@@ -95,3 +95,22 @@ $(document).ready(function() {
 	
 })
 
+function reply(said) {
+  switch(said.toLowerCase()){
+
+
+    case "hi":
+	case "hello":
+      possibleReplies = ["hi what can I help you with?", 'hi how are you doing today'];
+      break;
+	case "i'm doing good":
+      possibleReplies = ["that great! need help with anything?", "nice! keep it up!"];
+      break;
+   	case "i'm having a bad day":
+      possibleReplies = ["aww how can i make it better", "that's too bad hope you feel better"];
+      break;
+
+    default:
+      possibleReplies = ["sorry I don't understand, could you repeat that", "sorry could you say something else"]
+  }
+}
