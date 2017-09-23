dcdf2a4ed8b34b8ab74ca4ef34fa6523cd76f85f
patrickzebinghe <patrick2hb@hotmail.com>
2017-09-23 16:45:12 -0400

    Add files via upload

--- speak.js
+++ speak.js
@@ -45,7 +45,7 @@ function process(text) {
   console.log(text);
   reply(text)
   replies = possibleReplies
-  speak('hello')
+  console.log(replies)
   speak(replies[Math.random() * replies.length]);
   
 }
